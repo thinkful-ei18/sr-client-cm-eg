67db4ed6ea39ad7bfcca68022d2a0f39beee3010
Christina Moore <sayhitochristina@gmail.com>
2018-04-16 14:19:43 -0500

    write login form

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Field, reduxForm, focus } from 'redux-form';
 import { login } from './actions/auth'
 import { connect } from 'react-redux';
 
 
 
-export class LoginForm extends React.Component {
+export class LoginForm extends Component {
   render() {
 
     return (
       <div>
-        <h1>hi</h1>
-      </div>
+        <form>
+          <h1>Log in</h1>
+          <div className='fieldset-log'>
+            <label htmlFor='username'>Username</label>
+            <Field
+              component={Input}
+              type='text'
+              name='username'
+              validate={[required, notEmpty, isTrimmed]} />
+          </div>
+
+          <div className='fieldset-log'>
+            <label htmlFor='password'>Password</label>
+            <Field
+              component={Input}
+              type='password'
+              name='password'
+              validate={[required, notEmpty, isTrimmed]} />
+          </div>
+          <button type='submit' disabled={this.props.pristine || this.props.submitting}>Login</button>
+        </form >
+      </div >
+
     );
   }
 }
