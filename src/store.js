244e33573f74e55a08c998e24c48229b6f2da171
Christina Moore <sayhitochristina@gmail.com>
2018-04-16 13:20:18 -0500

    add redux chrome tool

--- src/store.js
+++ src/store.js
@@ -1,3 +1,24 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form'
 import thunk from 'redux-thunk';
+
+import authReducer from './reducers/authReducer';
+import { loadAuthToken } from './local-storage';
+import { setAuth } from './actions/auth';
+
+const store = createStore(
+  combineReducers({
+    form: formReducer,
+    auth: authReducer
+  }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+)
+
+const authToken = loadAuthToken();
+if (authToken) {
+  const token = authToken;
+  store.dispatch(setAuth(token));
+}
+
+export default store;
