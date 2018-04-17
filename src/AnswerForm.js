8c01867fdc91f8489c73a9ac9cb56e23b43fb049
Christina Moore <sayhitochristina@gmail.com>
2018-04-17 13:27:41 -0500

    begin redux answer form

---
+++ src/AnswerForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'tls';
+
+class AnswerForm extends Component {
+  render() {
+    return (
+      <div>hi</div>
+    )
+  }
+}
+
+export const mapStateToProps = (state, dispatch) => ({
+
+})
+
+export default connect(mapStateToProps)(AnswerForm);
