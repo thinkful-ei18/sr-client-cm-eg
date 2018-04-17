618b9696863092bb036ed2b3a7f7032696f80eb0
Christina Moore <sayhitochristina@gmail.com>
2018-04-17 10:54:30 -0500

    add requires login to dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import RequiresLogin from './Requires-Login';
 
 class Dashboard extends Component {
   render() {
@@ -13,4 +14,4 @@ export const mapStateToProps = (state, props) => ({
   loggedIn: state.auth.currentUser !== null
 });
 
-export default (connect(mapStateToProps)(Dashboard));
+export default RequiresLogin((connect(mapStateToProps)(Dashboard)));
