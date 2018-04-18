2c892f2e2da0efd7c7b50b613ad616b571c2543e
Evan Garrett <evang522@gmail.com>
2018-04-18 14:57:37 -0400

    Implemented Stats reducers, actions, begin modal for session reset.

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import RequiresLogin from './Requires-Login';
+import {fetchStats} from './actions/stats';
+import QuestionBox from './Questionbox';
 
 class Dashboard extends Component {
+
+
+  componentDidMount() {
+    this.props.dispatch(fetchStats());
+  }
   render() {
+    let questionStats;
+    if (this.props.questionScoreStats) {
+    questionStats = this.props.questionScoreStats.map((entry,index) => {
+      return ( <QuestionBox key={index} entry={entry}/>)
+    });
+  }
+
+
     return (
-      <h1>Correct: {this.props.correctInSession} Incorrect: {this.props.incorrectInSession}</h1>
+      <div>
+        <h1> This Session: </h1>
+        <br/>
+        <h1>Correct: {this.props.correctInSession} Incorrect: {this.props.incorrectInSession}</h1>
+        <br/>
+        <h1>Your Total Score: {this.props.totalUserScore}</h1>
+        <section className='question-score-container'>
+          <div className='question-score-header'>
+            Score by Questions:
+          </div>
+          <div className='questions-container'>
+            {questionStats}
+          </div>
+        </section>
+      </div>
     )
   }
 }
 
 export const mapStateToProps = (state, props) => ({
   correctInSession: state.stats.correct,
-  incorrectInSession: state.stats.incorrect
+  incorrectInSession: state.stats.incorrect,
+  totalUserScore: state.stats.totalUserScore,
+  questionScoreStats: state.stats.questionScoreStats
 })
 
 export default RequiresLogin()(connect(mapStateToProps)(Dashboard));
--- src/actions/stats.js
+++ src/actions/stats.js
@@ -1,7 +1,61 @@
 //TODO: write fetch for GET & POST
 //TODO: write actions
+import {API_BASE_URL} from '../config';
+import axios from 'axios';
 
 export const RESET_SESSION = 'RESET_SESSION';
 export const resetSession = () => ({
   type: RESET_SESSION
 })
+
+
+export const POPULATE_STATS = 'POPULATE_STATS';
+export const populateStats = (stats) => ({
+  type:POPULATE_STATS,
+  stats
+})
+
+export const SET_ERROR = 'SET_ERROR';
+export const setError = err => ({
+  type: SET_ERROR,
+  err
+})
+
+
+
+//================================== Async Actions ====================>
+
+
+export const incrementSessions = () => dispatch => {
+  const authToken = localStorage.getItem('authToken');
+  axios({
+    url:`${API_BASE_URL}/stats`,
+    method:'POST',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${authToken}`
+    },
+  })
+  .then(response => {
+    console.log('Congratulations, you completed a session!');
+  })
+  .catch(err => {
+    dispatch(setError(err));
+  })
+};
+
+export const fetchStats = () => dispatch => {
+  const authToken = localStorage.getItem('authToken');
+  axios({
+    url:`${API_BASE_URL}/stats`,
+    method:'GET',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${authToken}`
+    },
+  })
+  .then(response => {
+    console.log(response.data);
+    dispatch(populateStats(response.data));
+  })
+}
